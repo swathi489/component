54ed3cf9bed376d43acf2e4fb86f9c617c227595
swathi489 <140783082+swathi489@users.noreply.github.com>
2024-03-26 17:47:25 +0530

    Add files via upload

---
+++ Audioplayer/audio.js
@@ -0,0 +1,14 @@
+var audio = document.getElementById("audio");
+
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
+
+// function stopAudio() {
+//   audio.pause();
+//   audio.currentTime = 0;
+// }
