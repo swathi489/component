137e2d104f051e1aa69948b6a9c75197a3ad1e4f
swathi489 <140783082+swathi489@users.noreply.github.com>
2024-03-27 11:04:01 +0530

    Add files via upload

---
+++ Searchbar/searchbar.js
@@ -0,0 +1,4 @@
+document.getElementById('searchButton').addEventListener('click', function() {
+  var searchTerm = document.getElementById('searchInput').value;
+  console.log('Searching for: ' + searchTerm);
+});
